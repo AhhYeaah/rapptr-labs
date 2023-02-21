5c5ff843307134571f6988b9655b3d8fc98b85c4
AhhYeaah <vivi26743@hotmail.com>
2023-02-21 19:58:13 -0300

    made changes to make it more reusable

--- src/Components/Navbar/NavbarButtonGroup/NavbarButtonGroupLink/NavbarButtonGroupLink.tsx
+++ src/Components/Navbar/NavbarButtonGroup/NavbarButtonGroupLink/NavbarButtonGroupLink.tsx
@@ -3,6 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 interface NavbarButtonGroupLinkProps {
   to?: string;
+  colorIfActive?: boolean;
   children: string;
 }
 
@@ -10,13 +11,13 @@ function capitalizeFirstLetter(string: string) {
   return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
 
-export function NavbarButtonGroupLink({ children, to = children.toLowerCase() }: NavbarButtonGroupLinkProps) {
+export function NavbarButtonGroupLink({ children, to, colorIfActive }: NavbarButtonGroupLinkProps) {
   return (
     <li key={`NavbarButtonGroupLink - ${children}`} className="mx-4">
       <NavLink
-        to={'/' + to}
+        to={to ?? '/' + children.toLowerCase()}
         className={({ isActive }) => {
-          return isActive
+          return isActive && colorIfActive
             ? 'text-[#3ec7f4]'
             : '' +
                 ' hover:text-[#3ec7f4] active:text-white focus-visible:text-[#3ec7f4] text-b transition-all duration-150';

aa81509788a9ef803533b9802fe7644b2a632702
AhhYeaah <vivi26743@hotmail.com>
2023-02-20 21:49:33 -0300

    since almost all pages have a navbar and a footer, im creating a new element for it.

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,45 @@
 import React from 'react';
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { IndexPage } from './pages/Index/IndexPage';
+import { HomePage } from './pages/Home/HomePage';
+import { Page } from './pages/Page/Page';
 import { WorkPage } from './pages/Work/WorkPage';
 
 export function App() {
   const router = createBrowserRouter([
     {
       path: '/',
-      element: <IndexPage></IndexPage>,
-    },
-    {
-      path: '/work',
-      element: <WorkPage></WorkPage>,
-    },
-    {
-      path: '/approach',
-      element: <WorkPage></WorkPage>,
-    },
-    {
-      path: '/about',
-      element: <WorkPage></WorkPage>,
-    },
-    {
-      path: '/jobs',
-      element: <WorkPage></WorkPage>,
-    },
-    {
-      path: '/academy',
-      element: <WorkPage></WorkPage>,
-    },
-    {
-      path: '/contact-us',
-      element: <WorkPage></WorkPage>,
+      element: <Page />,
+      children: [
+        {
+          path: '/',
+          element: <HomePage></HomePage>,
+        },
+        {
+          path: '/work',
+          element: <WorkPage></WorkPage>,
+        },
+        {
+          path: '/approach',
+          element: <WorkPage></WorkPage>,
+        },
+        {
+          path: '/about',
+          element: <WorkPage></WorkPage>,
+        },
+        {
+          path: '/jobs',
+          element: <WorkPage></WorkPage>,
+        },
+        {
+          path: '/academy',
+          element: <WorkPage></WorkPage>,
+        },
+        {
+          path: '/contact-us',
+          element: <WorkPage></WorkPage>,
+        },
+      ],
     },
   ]);
   return (

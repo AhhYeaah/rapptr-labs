f1b130937576a886b0ac463ca37767bbe27f652a
AhhYeaah <vivi26743@hotmail.com>
2023-02-20 21:51:11 -0300

    now pages can focus only on their content instead of loading navbars and stuf

--- src/pages/Work/WorkPage.tsx
+++ src/pages/Work/WorkPage.tsx
@@ -8,10 +8,5 @@ interface WorkPageProps {
 }
 
 export function WorkPage({ value }: WorkPageProps) {
-  return (
-    <div className="h-[3000px] bg-slate-200">
-      <Navbar></Navbar>
-      <Outlet></Outlet>
-    </div>
-  );
+  return <div className="h-[3000px] bg-slate-200"></div>;
 }

11dd99b6de1a0385b907ce0256d67ac9b2f2f63d
AhhYeaah <vivi26743@hotmail.com>
2023-02-22 01:22:15 -0300

    extracting page title into a component

---
+++ src/pages/PageTitle/PageTitle.tsx
@@ -0,0 +1,13 @@
+import React, { ReactNode } from 'react';
+
+interface PageTitleProps {
+  children: string;
+}
+
+export function PageTitle({ children }: PageTitleProps) {
+  return (
+    <div className="flex-center text-center text-white text-[60px] font-extrabold h-[250px] w-full bg-[#0b131c]">
+      {children}
+    </div>
+  );
+}

8f78a5794805e3bb0ad235817ebc594b956c4ebf
AhhYeaah <vivi26743@hotmail.com>
2023-02-22 01:23:03 -0300

    using the new pagetitle component in the pages

---
+++ src/pages/SimplePage/Contact/ContactPage.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from 'react';
+import { PageTitle } from '../../PageTitle/PageTitle';
+
+export function ContactPage() {
+  return (
+    <div>
+      <PageTitle>Contact Us</PageTitle>
+      <div className="max-w-screen-xl mx-10 my-20 text-div">aaaaa</div>
+    </div>
+  );
+}
---
+++ src/pages/TextHeavyPages/TextHeavyPages.tsx
@@ -0,0 +1,25 @@
+import React, { ReactNode } from 'react';
+import { PageTitle } from '../PageTitle/PageTitle';
+import './TextHeavyPages.css';
+
+interface TextHeavyPagesProps {
+  children: ReactNode;
+  title: string;
+}
+
+export function TextHeavyPages({ title, children }: TextHeavyPagesProps) {
+  document.title = 'Rapptr Labs - ' + title;
+
+  return (
+    <div className="flex items-center flex-col grow">
+      <PageTitle>{title}</PageTitle>
+      <div className="max-w-screen-xl mx-10 my-20 text-div">
+        <div className="mb-12">
+          <h1 className="styled-title">{title}</h1>
+          <h2 className="text-center text-[#a7a9ac]">Last updated: March 13th, 2022</h2>
+        </div>
+        {children}
+      </div>
+    </div>
+  );
+}

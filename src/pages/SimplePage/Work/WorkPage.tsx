803b4de625776b732d1a075d07ac7bf5c14e3eea
AhhYeaah <vivi26743@hotmail.com>
2023-02-22 01:23:32 -0300

    removing useless imports

--- src/pages/SimplePage/Work/WorkPage.tsx
+++ src/pages/SimplePage/Work/WorkPage.tsx
@@ -1,6 +1,4 @@
 import React, { ReactNode } from 'react';
-import { Outlet } from 'react-router-dom';
-import { Navbar } from '../../../Components/Navbar/Navbar';
 
 interface WorkPageProps {
   value?: string;

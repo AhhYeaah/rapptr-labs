fa46594d001440b7b1ff046e7af5f1a342351824
AhhYeaah <vivi26743@hotmail.com>
2023-02-22 01:23:48 -0300

    moving to a better place

---
+++ src/pages/TextHeavyPages/PrivacyPolicy/PrivacyPolicyPage.tsx
@@ -0,0 +1,134 @@
+import React, { ReactNode } from 'react';
+
+// I had to generate another privacy policy, the one on the site was too long to manually
+// format and too messy to correct using code, sorry :/
+
+export function PrivacyPolicyPage() {
+  return (
+    <>
+      <h2 className="styled-subtitle">What does this Private Policy applys to?</h2>
+      <p className="styled-paragraph">
+        This Privacy Policy applies to your Personal Information collected or processed by D & A Technologies LLC d/b/a
+        Rapptr Labs, and/or its affiliated entities (collectively, “Rapptr Labs,” “we,” “us” or “our”), through their
+        websites or other sites that display or are linked to this Privacy Policy (collectively, the “Site” or “Sites”).
+        This Privacy Policy applies to your Personal Information, which can include information or a combination of
+        information that could directly or indirectly be used to identify you, in particular by reference to an
+        identifier such as a name, an email, login credentials, an online identifier, and any other information
+        collected from or about you (hereinafter, “Personal Information”). Please note, the definition of Personal
+        Information can vary by jurisdiction. If we collect information from you that is considered Personal Information
+        in your jurisdiction, we will treat it as such in accordance with this Privacy Policy.
+      </p>
+      <p className="styled-paragraph">
+        Our site may contain links to third-party websites and services that are not owned or controlled by Rapptr Labs.
+        We are not responsible for the privacy practices or the content of third-party websites, services,
+        advertisements, and you visit them at your own risk.
+      </p>
+      <p className="styled-paragraph">
+        Please read this Privacy Policy carefully to understand our policies and practices regarding your Personal
+        Information and how we will treat it. This Privacy Policy may change from time to time. We will notify you of
+        material changes to this Privacy Policy by posting a notice on our home page for a reasonable period of time and
+        changing the “Last Updated” legend at the top of this page So please check the Site periodically for updates
+      </p>
+
+      <p className="styled-paragraph">
+        The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which is accessible
+        at https://www.rapptr-labs.com, unless otherwise defined in this Privacy Policy.
+      </p>
+
+      <h2 className="styled-subtitle">Information Collection and Use</h2>
+
+      <p className="styled-paragraph">
+        For a better experience while using our Service, we may require you to provide us with certain personally
+        identifiable information, including but not limited to your name, phone number, and postal address. The
+        information that we collect will be used to contact or identify you.
+      </p>
+
+      <h2 className="styled-subtitle">Log Data</h2>
+
+      <p className="styled-paragraph">
+        We want to inform you that whenever you visit our Service, we collect information that your browser sends to us
+        that is called Log Data. This Log Data may include information such as your computer’s Internet Protocol
+        (&quot;IP&quot;) address, browser version, pages of our Service that you visit, the time and date of your visit,
+        the time spent on those pages, and other statistics.
+      </p>
+
+      <h2 className="styled-subtitle">Cookies</h2>
+
+      <p className="styled-paragraph">
+        Cookies are files with small amount of data that is commonly used an anonymous unique identifier. These are sent
+        to your browser from the website that you visit and are stored on your computer’s hard drive.
+      </p>
+
+      <p className="styled-paragraph">
+        Our website uses these &quot;cookies&quot; to collection information and to improve our Service. You have the
+        option to either accept or refuse these cookies, and know when a cookie is being sent to your computer. If you
+        choose to refuse our cookies, you may not be able to use some portions of our Service.
+      </p>
+
+      <h2 className="styled-subtitle">Service Providers</h2>
+
+      <p className="styled-paragraph">
+        We may employ third-party companies and individuals due to the following reasons:
+      </p>
+
+      <ul className="styled-paragraph">
+        <li className="styled-listitem">To facilitate our Service;</li>
+        <li className="styled-listitem">To provide the Service on our behalf;</li>
+        <li className="styled-listitem">To perform Service-related services; or</li>
+        <li className="styled-listitem">To assist us in analyzing how our Service is used.</li>
+      </ul>
+
+      <p className="styled-paragraph">
+        We want to inform our Service users that these third parties have access to your Personal Information. The
+        reason is to perform the tasks assigned to them on our behalf. However, they are obligated not to disclose or
+        use the information for any other purpose.
+      </p>
+
+      <h2 className="styled-subtitle">Security</h2>
+
+      <p className="styled-paragraph">
+        We value your trust in providing us your Personal Information, thus we are striving to use commercially
+        acceptable means of protecting it. But remember that no method of transmission over the internet, or method of
+        electronic storage is 100% secure and reliable, and we cannot guarantee its absolute security.
+      </p>
+
+      <h2 className="styled-subtitle">Links to Other Sites</h2>
+
+      <p className="styled-paragraph">
+        Our Service may contain links to other sites. If you click on a third-party link, you will be directed to that
+        site. Note that these external sites are not operated by us. Therefore, we strongly advise you to review the
+        Privacy Policy of these websites. We have no control over, and assume no responsibility for the content, privacy
+        policies, or practices of any third-party sites or services.
+      </p>
+
+      <p className="styled-paragraph">Children&apos;s Privacy</p>
+
+      <p className="styled-paragraph">
+        Our Services do not address anyone under the age of 13. We do not knowingly collect personal identifiable
+        information from children under 13. In the case we discover that a child under 13 has provided us with personal
+        information, we immediately delete this from our servers. If you are a parent or guardian and you are aware that
+        your child has provided us with personal information, please contact us so that we will be able to do necessary
+        actions.
+      </p>
+
+      <h2 className="styled-subtitle">Changes to This Privacy Policy</h2>
+
+      <p className="styled-paragraph">
+        We may update our Privacy Policy from time to time. Thus, we advise you to review this page periodically for any
+        changes. We will notify you of any changes by posting the new Privacy Policy on this page. These changes are
+        effective immediately, after they are posted on this page.
+      </p>
+
+      <p className="styled-paragraph">
+        Our Privacy Policy was created with the help of the
+        <a href="https://www.termsfeed.com/blog/sample-privacy-policy-template/">TermsFeed Privacy Policy Template</a>.
+      </p>
+
+      <h2 className="styled-subtitle">Contact Us</h2>
+
+      <p className="styled-paragraph">
+        If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us.
+      </p>
+    </>
+  );
+}

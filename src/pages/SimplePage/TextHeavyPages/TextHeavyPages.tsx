96a53a3dbe86aa25bd1265881869d4974dcd8f9a
AhhYeaah <vivi26743@hotmail.com>
2023-02-22 00:43:14 -0300

    changed spacing so title doesnt need to do it anymore

--- src/pages/SimplePage/TextHeavyPages/TextHeavyPages.tsx
+++ src/pages/SimplePage/TextHeavyPages/TextHeavyPages.tsx
@@ -8,14 +8,13 @@ interface TextHeavyPagesProps {
 }
 
 export function TextHeavyPages({ title, children }: TextHeavyPagesProps) {
-  const changeIsNavbarDark = useOutletContext<(value: boolean) => void>();
   document.title = 'Rapptr Labs - ' + title;
 
   return (
     <div className="flex items-center flex-col grow">
       <div className="flex-center text-white text-[60px] font-bold h-[250px] w-full bg-[#0b131c]">{title}</div>
-      <div className="max-w-screen-xl m-10 text-div">
-        <div>
+      <div className="max-w-screen-xl mx-10 my-20 text-div">
+        <div className="mb-10">
           <h1 className="styled-title">{title}</h1>
           <h2 className="text-center text-[#a7a9ac]">Last updated: March 13th, 2022</h2>
         </div>

d3f943f282d294ac470f5d1ce9f573142b614a73
AhhYeaah <vivi26743@hotmail.com>
2023-02-28 22:14:23 -0300

    Removing everything and starting over

--- src/pages/SimplePage/Home/HomePage.tsx
+++ src/pages/SimplePage/Home/HomePage.tsx
@@ -1,16 +1,5 @@
 import React from 'react';
-import { useOutletContext } from 'react-router-dom';
 
 export function HomePage() {
-  const changeIsNavbarDark = useOutletContext<(value: boolean) => void>();
-  changeIsNavbarDark(false);
-
-  return (
-    <div className="grow bg-red-200 flex-center">
-      <div className="">
-        <h1>We build apps that</h1>
-      </div>
-      We build apps that cativate
-    </div>
-  );
+  return <div className="bg-slate-200 flex-center w-full"></div>;
 }

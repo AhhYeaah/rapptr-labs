0e251a8a9593c90db8163b81e648a1f65d6d55e1
AhhYeaah <vivi26743@hotmail.com>
2023-03-06 16:29:41 -0300

    making so that every page inside simplepages stretches until footer reaches bottom of page

--- src/pages/SimplePage/SimplePage.tsx
+++ src/pages/SimplePage/SimplePage.tsx
@@ -2,15 +2,16 @@ import React, { ReactNode, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Footer } from '../../Components/Footer/Footer';
 import { Navbar } from '../../Components/Navbar/Navbar';
+import './SimplePage.css';
 
 export function SimplePage() {
   //state lift
   const [isNavBarDark, changeIsNavbarDark] = useState(false);
   window.scrollTo({ top: 0, behavior: 'smooth' });
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen ">
       <Navbar darkMode={isNavBarDark}></Navbar>
-      <main className="grow flex items-stretch">
+      <main className="mainPageContainer flex grow items-stretch">
         <Outlet context={changeIsNavbarDark}></Outlet>
       </main>
       <Footer></Footer>

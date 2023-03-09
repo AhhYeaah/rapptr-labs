9b30c839ce1504953ff35cf719b9f299d8750b15
AhhYeaah <vivi26743@hotmail.com>
2023-03-09 02:12:41 -0300

    REFACTOR: improveed readbility,

--- src/pages/SimplePage/Contact/Form/ContactFormInput/ContactFormPhoneInput/ContactFormPhoneInput.tsx
+++ src/pages/SimplePage/Contact/Form/ContactFormInput/ContactFormPhoneInput/ContactFormPhoneInput.tsx
@@ -1,40 +1,47 @@
 import 'react-phone-number-input/style.css';
 import './ContactFormPhoneInput.css';
-import PhoneInput from 'react-phone-number-input';
+import PhoneInput, { isValidPhoneNumber } from 'react-phone-number-input';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 interface ContactFormPhoneInput {
   name: string;
-  placeholder?: string;
   id: string;
+  required: boolean;
+  register: any;
+  placeholder?: string;
 }
 
-export function ContactFormPhoneInput({ name, id, placeholder = 'Phone' }: ContactFormPhoneInput) {
+export function ContactFormPhoneInput({ register, ...props }: ContactFormPhoneInput) {
   const [countryCode, changeCountryCode] = useState();
   const [value, onChange] = useState('');
 
   useEffect(() => {
-    console.log('eh');
     //smart default
-    axios
-      .get('http://ip-api.com/json', { timeout: 2000 })
-      .then((countryCode) => {
-        changeCountryCode(countryCode.data.countryCode);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    function getUserCountry() {
+      axios
+        .get('http://ip-api.com/json', { timeout: 2000 })
+        .then((countryCode) => {
+          changeCountryCode(countryCode.data.countryCode);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+
+    getUserCountry();
   }, []);
 
   return (
     <div>
       <PhoneInput
         className="border outline-none focus-within:border-[#2984a2] border-rapptr-blue w-full p-3 flex align-center rounded-sm h-10 phoneBorder"
-        placeholder={placeholder}
-        name={name}
-        id={name}
         defaultCountry={countryCode}
+        {...props}
+        {...register(props.name, {
+          setValueAs: () => value,
+          validate: () => isValidPhoneNumber(value) || 'Invalid Phone length',
+        })}
         value={value}
         onChange={onChange}
       />

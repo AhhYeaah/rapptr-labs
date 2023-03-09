dde2e68e3e54520ec68067cf41d0b5b7d27fbfea
AhhYeaah <vivi26743@hotmail.com>
2023-03-09 06:07:46 -0300

    BUGFIX: Using controller to apply validation since this is a controled input

--- src/pages/SimplePage/Contact/Form/ContactFormInput/ContactFormPhoneInput/ContactFormPhoneInput.tsx
+++ src/pages/SimplePage/Contact/Form/ContactFormInput/ContactFormPhoneInput/ContactFormPhoneInput.tsx
@@ -1,20 +1,21 @@
 import 'react-phone-number-input/style.css';
 import './ContactFormPhoneInput.css';
 import PhoneInput, { isValidPhoneNumber } from 'react-phone-number-input';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, createElement } from 'react';
 import axios from 'axios';
+import { Controller, useController } from 'react-hook-form';
 
 interface ContactFormPhoneInput {
   name: string;
   id: string;
-  required: boolean;
   register: any;
+  control: any;
+  hasError: boolean;
   placeholder?: string;
 }
 
-export function ContactFormPhoneInput({ register, ...props }: ContactFormPhoneInput) {
+export function ContactFormPhoneInput({ hasError, register, control, ...props }: ContactFormPhoneInput) {
   const [countryCode, changeCountryCode] = useState();
-  const [value, onChange] = useState('');
 
   useEffect(() => {
     //smart default
@@ -32,18 +33,31 @@ export function ContactFormPhoneInput({ register, ...props }: ContactFormPhoneIn
     getUserCountry();
   }, []);
 
+  const { field } = useController({
+    name: props.name,
+    control,
+    rules: {
+      required: 'This field is required',
+      validate: (value) => isValidPhoneNumber(value) || 'Invalid Phone Number length',
+    },
+  });
+
   return (
     <div>
       <PhoneInput
-        className="border outline-none focus-within:border-[#2984a2] border-rapptr-blue w-full p-3 flex align-center rounded-sm h-10 phoneBorder"
+        className={
+          `border bg-transparent focus-within:outline-none h-10 focus-within:ring-1 w-full px-3 appearance-none flex align-center rounded-sm ` +
+          (hasError
+            ? 'border-red-500 text-red-600 focus-within:border-red-500 focus-within:ring-red-500 '
+            : 'border-rapptr-blue focus-within:ring-[#2984a2] focus-within:border-[#2984a2] ')
+        }
         defaultCountry={countryCode}
         {...props}
-        {...register(props.name, {
-          setValueAs: () => value,
-          validate: () => isValidPhoneNumber(value) || 'Invalid Phone length',
-        })}
-        value={value}
-        onChange={onChange}
+        onChange={(value) => field.onChange(value ?? '')} // send value to hook form
+        onBlur={field.onBlur} // notify when input is touched/blur
+        value={field.value} // input value
+        name={field.name} // send down the input name
+        inputRef={field.ref} // send input ref, so we can focus on input when error appear
       />
     </div>
   );

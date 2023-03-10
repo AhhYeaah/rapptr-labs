9f1f3ba633fc4ef9702d49056ff1eb26de5102a8
AhhYeaah <vivi26743@hotmail.com>
2023-03-09 23:19:28 -0300

    REFACTOR: Now button doesnt need a onClick value

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -4,13 +4,22 @@ interface ButtonProps {
   className?: string;
   hasDefaultSizing?: boolean;
   disabled?: boolean;
-  onClick: () => void;
+  onClick?: () => void;
   children?: ReactNode[] | ReactNode;
 }
 
-export function Button({ className, children, onClick, disabled = false }: ButtonProps) {
+export function Button({
+  className,
+  children,
+  onClick,
+  disabled = false,
+}: ButtonProps) {
   return (
-    <button onClick={onClick} disabled={disabled} className={'py-2 px-6  disabled:opacity-50 ' + className}>
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      className={'py-2 px-6  disabled:opacity-50 ' + className}
+    >
       {children}
     </button>
   );

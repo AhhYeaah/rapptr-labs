c308214cdfcdf6de435f06556ab2deda9ad40ca4
AhhYeaah <vivi26743@hotmail.com>
2023-03-14 17:27:44 -0300

    LINTING.

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -16,7 +16,7 @@ export function Footer() {
   const navigate = useNavigate();
 
   return (
-    <footer className="bg-rapptr-black text-white grow-0 px-4 flex-center flex-col h-auto lg:h-96">
+    <footer className="bg-rapptr-black text-white grow-0 px-4 flex-center flex-col h-auto lg:h-96 snap-end">
       <div className="py-14 w-auto lg:py-0 lg:w-full lg:max-w-screen-lg">
         <div className="w-auto grid grid-rows-3 grid-cols-1 gap-10 lg:w-full  lg:grid-rows-1 lg:grid-cols-10 lg:gap-0">
           <div className="flex-center lg:col-span-2 lg:block">
@@ -28,9 +28,7 @@ export function Footer() {
               hasMargin={false}
               className="flex-col"
             >
-              <NavbarButtonGroupLink to="/">
-                Home
-              </NavbarButtonGroupLink>
+              <NavbarButtonGroupLink to="/">Home</NavbarButtonGroupLink>
               <NavbarButtonGroupLink>Work</NavbarButtonGroupLink>
               <NavbarButtonGroupLink>Approach</NavbarButtonGroupLink>
               <NavbarButtonGroupLink>About</NavbarButtonGroupLink>
@@ -44,22 +42,14 @@ export function Footer() {
               hasMargin={false}
               className="flex-col"
             >
-              <NavbarButtonGroupLink to="">
-                App Design
-              </NavbarButtonGroupLink>
-              <NavbarButtonGroupLink>
-                App Development
-              </NavbarButtonGroupLink>
-              <NavbarButtonGroupLink>
-                Android App Development
-              </NavbarButtonGroupLink>
+              <NavbarButtonGroupLink to="">App Design</NavbarButtonGroupLink>
+              <NavbarButtonGroupLink>App Development</NavbarButtonGroupLink>
+              <NavbarButtonGroupLink>Android App Development</NavbarButtonGroupLink>
             </NavbarButtonGroup>
           </nav>
-          <div className=" text-center lg:text-left lg:col-span-3">
+          <div className="text-center lg:text-left lg:col-span-3">
             <div>
-              <h2 className="mb-2 text-rapptr-blue text-lg">
-                Rapptr Labs
-              </h2>
+              <h2 className="mb-2 text-rapptr-blue text-lg">Rapptr Labs</h2>
               <address className="mb-3 not-italic	 ">
                 <p>
                   185 Hudson St, Suite 2340 <br />

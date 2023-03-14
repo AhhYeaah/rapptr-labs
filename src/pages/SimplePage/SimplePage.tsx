8d763cddacb6372a915a3ced0d9ac361d0f44ed9
AhhYeaah <vivi26743@hotmail.com>
2023-03-14 17:29:01 -0300

    REFACTOR: Eazy way to show wich pages will have a dark navbar.

--- src/pages/SimplePage/SimplePage.tsx
+++ src/pages/SimplePage/SimplePage.tsx
@@ -1,19 +1,20 @@
-import React, { useState } from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { ReactNode } from 'react';
 import { Footer } from '../../Components/Footer/Footer';
 import { Navbar } from '../../Components/Navbar/Navbar';
 import './SimplePage.css';
 
-export function SimplePage() {
-  //state lift
-  const [isNavBarDark, changeIsNavbarDark] = useState(false);
+interface SimplePageProp {
+  children: ReactNode;
+  isNavBarDark?: boolean;
+}
+
+export function SimplePage({ children: page, isNavBarDark = false }: SimplePageProp) {
   window.scrollTo({ top: 0, behavior: 'smooth' });
+
   return (
-    <div className="flex flex-col min-h-screen ">
+    <div className="flex flex-col min-h-screen">
       <Navbar darkMode={isNavBarDark}></Navbar>
-      <main className="mainPageContainer flex grow items-stretch">
-        <Outlet context={changeIsNavbarDark}></Outlet>
-      </main>
+      <main className="mainPageContainer flex grow items-stretch ">{page}</main>
       <Footer></Footer>
     </div>
   );

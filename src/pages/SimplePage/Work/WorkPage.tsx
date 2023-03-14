3eaecb7d6e7d6baccadb8213a89bc2d415b7b61f
AhhYeaah <vivi26743@hotmail.com>
2023-03-14 17:34:07 -0300

    FEATURE: WIP, new work page.

--- src/pages/SimplePage/Work/WorkPage.tsx
+++ src/pages/SimplePage/Work/WorkPage.tsx
@@ -1,26 +1,67 @@
-import React, { ReactNode } from 'react';
-import { Carrousel } from './Carrousel/Carrousel';
-import { CarrouselImage } from './Carrousel/CarrouselImage/CarrouselImage';
+import React from 'react';
+import { Blur } from '../../../Components/Animations/Blur/Blur';
+import { FadeIn } from '../../../Components/Animations/FadeIn/FadeIn';
+import { SlideUp } from '../../../Components/Animations/SlideUp/SlideUp';
+import { DarkButton } from '../../../Components/Button/DarkButton';
 
-interface WorkPageProps {
-  value?: string;
-  children?: ReactNode[] | ReactNode;
-}
-
-export function WorkPage({ value }: WorkPageProps) {
+export function WorkPage() {
   return (
-    <div className="bg-black overflow-hidden">
-      <Carrousel>
-        <CarrouselImage></CarrouselImage>
-        <CarrouselImage></CarrouselImage>
-        <CarrouselImage></CarrouselImage>
-        <CarrouselImage></CarrouselImage>
-        <CarrouselImage></CarrouselImage>
-        <CarrouselImage></CarrouselImage>
-        <CarrouselImage></CarrouselImage>
-        <CarrouselImage></CarrouselImage>
-        <CarrouselImage></CarrouselImage>
-      </Carrousel>
+    <div className="snappable-page flex-col">
+      <div className="relative flex-center h-[90vh] bg-gradient-to-t from-[#121f2e] to-black ">
+        <div className="absolute bottom-0 top-0 opacity-30 snap-end">
+          <SlideUp duration={1000} pixelsTop={500}>
+            <FadeIn duration={1000}>
+              <Blur blurInPixels={5} duration={800} delay={1500}>
+                <img
+                  className="h-[80vh] select-none"
+                  draggable={false}
+                  src="https://www.rapptrlabs.com/static/Frame1-5203f3e9d000eb2f056e6334699dd91e.png"
+                  alt=""
+                />
+              </Blur>
+            </FadeIn>
+          </SlideUp>
+        </div>
+        <div>
+          <FadeIn duration={1000} delay={1500}>
+            <div>
+              <div className={'flex-center text-center text-white w-full mb-20'}>
+                <div className="max-w-screen-xl ">
+                  <div className="text-7xl font-extrabold w-auto block">
+                    Bringing <span className="text-rapptr-blue">Digital Products</span> to
+                    Life
+                  </div>
+
+                  <div className="text-2xl w-auto mt-10 font-light">
+                    From idealization to development, the Rapptr team has helped dozens of
+                    clients build and launch custom mobile and web apps and premium
+                    digital experiences. Our clients include companies of all sizes, from
+                    high-growth startups to enterprise brands and everything in between.
+                  </div>
+                  <div>
+                    <DarkButton className="mt-5">Take a look at our works</DarkButton>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </FadeIn>
+        </div>
+      </div>
+
+      <div className="flex-center h-screen bg-[#121f2e] snap-end">
+        {/* <Carrousel>
+          <CarrouselImage></CarrouselImage>
+          <CarrouselImage></CarrouselImage>
+          <CarrouselImage></CarrouselImage>
+          <CarrouselImage></CarrouselImage>
+          <CarrouselImage></CarrouselImage>
+          <CarrouselImage></CarrouselImage>
+          <CarrouselImage></CarrouselImage>
+          <CarrouselImage></CarrouselImage>
+          <CarrouselImage></CarrouselImage>
+          <CarrouselImage></CarrouselImage>
+        </Carrousel> */}
+      </div>
     </div>
   );
 }

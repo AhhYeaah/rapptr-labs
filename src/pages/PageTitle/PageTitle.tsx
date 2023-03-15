571edb4c5d97f6faae2e930b8e2687b200350faa
AhhYeaah <vivi26743@hotmail.com>
2023-03-14 23:38:14 -0300

    FEATURE: Made PageTItle size responsive!

--- src/pages/PageTitle/PageTitle.tsx
+++ src/pages/PageTitle/PageTitle.tsx
@@ -6,7 +6,7 @@ interface PageTitleProps {
 
 export function PageTitle({ children }: PageTitleProps) {
   return (
-    <div className="flex-center text-center text-white text-6xl font-extrabold h-[250px] w-full bg-[#0b131c]">
+    <div className="flex-center text-center text-white text-4xl lg:text-6xl font-extrabold py-[10vh]  w-full bg-[#0b131c]">
       {children}
     </div>
   );
